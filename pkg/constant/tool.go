b9aca8cdc2abb3382c684abf006cde445cf3bad1
lms <1359816810@qq.com>
2024-06-12 10:46:24 +0800

    feat(tool): 添加tool监听路径和端口

---
+++ pkg/constant/tool.go
@@ -0,0 +1,10 @@
+package constant
+
+const (
+	// ToolDefaultUnixSocket Path for the unix socket.
+	// daemon by default always listens on the default unix socket
+	ToolDefaultUnixSocket = "/var/run/cosslan.sock"
+
+	// ToolDefaultPort daemon listen port
+	ToolDefaultPort = 6976
+)
