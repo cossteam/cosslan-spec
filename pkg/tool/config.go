0728a2f39af7e03b30dcee3132759c77119df737
lms <1359816810@qq.com>
2024-06-13 11:27:46 +0800

    feat(tool): 添加config定义

---
+++ pkg/tool/config.go
@@ -0,0 +1,82 @@
+package tool
+
+import (
+	"fmt"
+	"time"
+)
+
+type Config struct {
+	StaticAddrMap map[string][]string `yaml:"staticAddrMap"`
+	Node          *NodeConfig         `yaml:"node"`
+	Listen        *ListenConfig       `yaml:"listen"`
+	Device        *DeviceConfig       `yaml:"device"`
+	Handshake     *HandshakeConfig    `yaml:"handshake"`
+	Firewall      *FirewallConfig     `yaml:"firewall"`
+}
+
+type NodeConfig struct {
+	AmNode bool     `yaml:"amNode"`
+	Addrs  []string `yaml:"addrs"`
+}
+
+type ListenConfig struct {
+	Host        string `yaml:"host"`
+	Port        int    `yaml:"port"`
+	Batch       int    `yaml:"batch"`
+	ReadBuffer  int    `yaml:"readBuffer"`
+	WriteBuffer int    `yaml:"writeBuffer"`
+	Routines    int    `yaml:"routines"`
+}
+
+type DeviceConfig struct {
+	Type string `yaml:"type"`
+	// Dev Name of the device. If not set, a default will be chosen by the OS.
+	// For macOS: if set, must be in the form `utun[0-9]+`.
+	// For NetBSD: Required to be set, must be in the form `tun[0-9]+`
+	Name               string `yaml:"name"`
+	IP                 string `yaml:"ip"`
+	Mask               string `yaml:"mask"`
+	DropLocalBroadcast bool   `yaml:"dropLocalBroadcast"`
+	DropMulticast      bool   `yaml:"dropMulticast"`
+	MTU                int    `yaml:"mtu"`
+}
+
+// HandshakeConfig 握手配置
+type HandshakeConfig struct {
+	SyncDevice    time.Duration `yaml:"syncDevice"`    // 设备同步时间
+	SyncNode      time.Duration `yaml:"syncNode"`      // 节点同步时间
+	TryInterval   time.Duration `yaml:"tryInterval"`   // 尝试间隔
+	Retries       int           `yaml:"retries"`       // 尝试次数
+	TriggerBuffer int           `yaml:"triggerBuffer"` // 触发缓冲
+}
+
+type FirewallConfig struct {
+	OutboundDefaultAction string         `yaml:"outboundDefaultAction"`
+	InboundDefaultAction  string         `yaml:"inboundDefaultAction"`
+	Outbound              []OutboundRule `yaml:"outbound"`
+	Inbound               []InboundRule  `yaml:"inbound"`
+}
+
+type OutboundRule struct {
+	Port  string   `yaml:"port"`
+	Proto string   `yaml:"proto"`
+	Host  []string `yaml:"host"`
+	// "allow" or "deny"
+	Action string `yaml:"action"`
+}
+
+type InboundRule struct {
+	Port  string   `yaml:"port"`
+	Proto string   `yaml:"proto"`
+	Host  []string `yaml:"host"`
+	// "allow" or "deny"
+	Action string `yaml:"action"`
+}
+
+func (r OutboundRule) String() string {
+	return fmt.Sprintf("port=%v proto=%s hosts=%v action=%s", r.Port, r.Proto, r.Host, r.Action)
+}
+
+func (r InboundRule) String() string {
+	return fmt.Sprintf("port=%v proto=%s hosts=%v action=%s", r.Port, r.Proto, r.Host, r.Action)
+}
